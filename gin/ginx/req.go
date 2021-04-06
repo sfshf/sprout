4803ef3ce768a8ad4d03d4d67f783389c2124ed5
sfshf <z1614789743@outlook.com>
2021-04-06 21:20:12 +0800

    first commit of gin contexts.

---
+++ gin/ginx/req.go
@@ -0,0 +1,15 @@
+package ginx
+
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+func GetClientRealIp(c *gin.Context) string {
+	if ip := c.GetHeader("X-Real-Ip"); ip != "" {
+		return ip
+	} else if ip = c.GetHeader("X-Forwarded-For"); ip != "" {
+		return ip
+	}
+	return strings.Split(c.Request.RemoteAddr, ":")[0]
+}
