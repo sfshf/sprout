6fdceae9bd3c75164a6ef6a32e998289b8d5bb9e
sfshf <z1614789743@outlook.com>
2021-04-06 21:22:57 +0800

    first commit of the app config files.

---
+++ app/govern/conf/config.go
@@ -0,0 +1,127 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/sfshf/sprout/pkg/util/json"
+	"github.com/spf13/viper"
+	"os"
+	"time"
+)
+
+var (
+	C config
+)
+
+func Init() {
+	fpath := os.Getenv("CONF")
+	if fpath == "" {
+		fpath = "app/govern/conf/config.toml"
+	}
+	viper.Reset()
+	viper.SetConfigFile(fpath)
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	if err := viper.Unmarshal(&C); err != nil {
+		panic(err)
+	}
+	if C.PrintConfig {
+		fmt.Printf("%s\n", json.MarshalIndent2String(C))
+	}
+}
+
+type config struct {
+	RunMode     string
+	WWW         string
+	Swagger     bool
+	PrintConfig bool
+	Global      global
+	Root        *root
+	MongoDB     mongoDB
+	JWTAuth     jwtAuth
+	Casbin      casbin
+	PicCaptcha  picCaptcha
+	HTTP        http
+	Log         log
+	CORS        cors
+	Redis       redis
+}
+
+type global struct {
+	AppName        string
+	AppIcon        string
+	DateFormat     string
+	DatetimeFormat string
+	TimeZone       string
+}
+
+type root struct {
+	Account   string
+	Password  string
+	SessionId string
+}
+
+type mongoDB struct {
+	ClientUri string
+	Database  string
+}
+
+type jwtAuth struct {
+	Enable     bool
+	SigningKey string
+	Expired    int64
+}
+
+type casbin struct {
+	Enable           bool
+	Debug            bool
+	Model            string
+	AutoLoad         bool
+	AutoLoadInternal int
+}
+
+type picCaptcha struct {
+	Enable      bool
+	Length      int
+	Width       int
+	Height      int
+	MaxSkew     float64
+	DotCount    int
+	Threshold   int
+	Expiration  time.Duration
+	RedisStore  bool
+	RedisDB     int
+	RedisPrefix string
+}
+
+type http struct {
+	Host             string
+	Port             int
+	CertFile         string
+	KeyFile          string
+	ShutdownTimeout  int
+	MaxContentLength int
+	MaxLoggerLength  int
+}
+
+type log struct {
+	SkipStderr      bool
+	Log2Mongo       bool
+	MongoCollection string
+	MaxWorkers      int
+	MaxBuffers      int
+}
+
+type cors struct {
+	Enable           bool
+	AllowOrigins     []string
+	AllowMethods     []string
+	AllowHeaders     []string
+	AllowCredentials bool
+	MaxAge           int
+}
+
+type redis struct {
+	Addr     string
+	Passwrod string
+}
