f1cb5c1bd16170a2d1ab8e01b32e9990970fc9fa
sfshf <z1614789743@outlook.com>
2021-04-09 13:38:19 +0800

    update docker configs.

--- docker-govern.sh
+++ docker-govern.sh
@@ -2,9 +2,25 @@
 
 set -eo pipefail
 
+# Remove the old container named govern-mongo, if has.
+docker rm -f govern-mongo
+# Run a new container named govern-mongo.
+docker run -itdp 27017:27017 \
+--name govern-mongo \
+-v govern-mongo-data-db:/data/db \
+-v govern-mongo-data-configdb:/data/configdb \
+--network sprout \
+mongo
+
 # Build a govern app image using the specific Dockerfile.
-docker build -t govern:latest -f app/govern/Dockerfile .
+docker build -t govern:latest \
+-f app/govern/Dockerfile .
 # Remove the old container named govern, if has.
 docker rm -f govern
 # Run a new container named govern.
-docker run -itdp 8080:8080 --network sprout --name govern govern
+docker run -itd -p 8080:8080 \
+-p 8090:8090 \
+--network sprout \
+--name govern \
+govern
+
